e1e84624a7209f9a828c03681b643d4cb8553ea5
Karina Białek <karinabialek2@gmail.com>
2021-04-06 15:10:53 +0200

    display position

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+function createPositionContainer(containerSelector) {
+
+    const container = document.querySelector(containerSelector)
+
+    if (!container) return
+
+    const div = document.createElement('div')
+
+    div.style.position = 'absolute'
+    div.style.bottom = '0px'
+    div.style.right = '0px'
+
+    container.appendChild(div)
+
+    return div
+}
+
 function createCircle(containerSelector) {
 
     const container = document.querySelector(containerSelector)
@@ -20,7 +37,7 @@ function createCircle(containerSelector) {
 }
 
 function extractPixelsFromString(pixelsString) {
-  return Number(pixelsString.replace('px', ''))
+    return Number(pixelsString.replace('px', ''))
 }
 
 function move(deltaX = 0, deltaY = 0) {
@@ -36,13 +53,27 @@ function moveDown(deltaTop = 10) {
 }
 
 function moveRight(deltaLeft = 10) {
-   move(deltaLeft, 0)
+    move(deltaLeft, 0)
 }
 function moveUp(deltaTop = 10) {
     move(0, -deltaTop)
 }
 function moveLeft(deltaLeft = 10) {
-  move(-deltaLeft, 0)
+    move(-deltaLeft, 0)
 }
 
+function displayPosition() {
+    const position = circle.getBoundingClientRect()
+
+    const pX = document.createElement('p')
+    const pY = document.createElement('p')
+
+    pX.innerText = 'Position X: ' + position.x
+    pY.innerText = 'Position X: ' + position.y
+
+    container.appendChild(pX)
+    container.appendChild(pY)
+}
+const container = createPositionContainer('body')
 const circle = createCircle('body')
+displayPosition()
