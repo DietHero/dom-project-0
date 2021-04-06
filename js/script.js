1b71ab70f43840f3faad1fd82483e5351179be33
Karina Białek <karinabialek2@gmail.com>
2021-04-06 12:45:10 +0200

    function createCircle added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
-const circle = document.createElement('div')
-circle.style.width = "100px"
-circle.style.height = "100px"
-circle.style.backgroundColor = "blue"
-circle.style.borderRadius = "50%"
+function createCircle(containerSelector) {
 
-document.body.appendChild(circle)
+    const container = document.querySelector(containerSelector)
 
+    if (!container) return
+
+    const circle = document.createElement('div')
+
+    circle.style.position = "absolut"
+    circle.style.top = "0px"
+    circle.style.left = "0px"
+    circle.style.width = "100px"
+    circle.style.height = "100px"
+    circle.style.backgroundColor = "blue"
+    circle.style.borderRadius = "50%"
+
+    container.appendChild(circle)
+
+    return circle
+}
+
+const circle1 = createCircle("body")
