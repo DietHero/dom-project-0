eae235d179adffd50974ace85b79d8db62ed8799
Karina Białek <karinabialek2@gmail.com>
2021-04-06 13:26:16 +0200

    move function refactor

--- js/script.js
+++ js/script.js
@@ -6,35 +6,43 @@ function createCircle(containerSelector) {
 
     const circle = document.createElement('div')
 
-    circle.style.position = "absolut"
-    circle.style.top = "0px"
-    circle.style.left = "0px"
-    circle.style.width = "100px"
-    circle.style.height = "100px"
-    circle.style.backgroundColor = "blue"
-    circle.style.borderRadius = "50%"
+    circle.style.position = 'absolute'
+    circle.style.top = '0px'
+    circle.style.left = '0px'
+    circle.style.width = '100px'
+    circle.style.height = '100px'
+    circle.style.backgroundColor = 'blue'
+    circle.style.borderRadius = '50%'
 
     container.appendChild(circle)
 
     return circle
 }
 
+function extractPixelsFromString(pixelsString) {
+  return Number(pixelsString.replace('px', ''))
+}
+
+function move(deltaX = 0, deltaY = 0) {
+    const currentTop = extractPixelsFromString(circle.style.top)
+    const currentLeft = extractPixelsFromString(circle.style.left)
+    circle.style.top = currentTop + deltaY + 'px'
+    circle.style.left = currentLeft + deltaX + 'px'
+}
+
+
 function moveDown(deltaTop = 10) {
-    const currentTop = Number(circle.style.top.replace('px', ' '))
-    circle.style.top = currentTop + deltaTop + "px"
+    move(0, deltaTop)
 }
 
 function moveRight(deltaLeft = 10) {
-    const currentLeft = Number(circle.style.left.replace('px', ' '))
-    circle.style.left = currentLeft + deltaLeft + "px"
+   move(deltaLeft, 0)
 }
 function moveUp(deltaTop = 10) {
-    const currentTop = Number(circle.style.top.replace('px', ' '))
-    circle.style.top = currentTop - deltaTop + "px"
+    move(0, -deltaTop)
 }
 function moveLeft(deltaLeft = 10) {
-    const currentLeft = Number(circle.style.left.replace('px', ' '))
-    circle.style.left = currentLeft - deltaLeft + "px"
+  move(-deltaLeft, 0)
 }
 
-const circle = createCircle("body")
+const circle = createCircle('body')
